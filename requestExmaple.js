1c3234aa80ffd103b0fdfc9b899f8c9cb12407ca
gwanwoo-you <skymill2000@gmail.com>
2020-11-17 15:12:21 +0900

    json parse 완성

--- requestExmaple.js
+++ requestExmaple.js
@@ -1,4 +1,9 @@
 const request = require('request');
 request('http://newsapi.org/v2/top-headlines?country=kr&category=health&apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5', function (error, response, body) {
-  console.log('body:', body); 
+  var parsedJson = JSON.parse(body);
+  var articles = parsedJson.articles;
+  articles.map((art) => {
+    console.log(art.title);
+  })
+
 });
