f940e3673278205c84f9032fdfcbf2ae5414ccda
gwanwoo-you <skymill2000@gmail.com>
2020-11-17 15:09:09 +0900

    날씨 요청 API 완성

--- weather.js
+++ weather.js
@@ -4,6 +4,7 @@ var parseString = require('xml2js').parseString;
 var url = "http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109";
 request(url, function (error, response, body) {
     parseString(body, function (err, result) {
-        console.dir(result);
+        console.dir(result.rss.channel[0].item[0].description[0].header[0].wf[0]);
+        //#work4 예보정보 wf 안에 있는 데이터를 출력
     });   
 });
