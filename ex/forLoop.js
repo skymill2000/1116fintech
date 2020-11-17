bcfa877c60c4265b572df4bb239f143efe909e92
gwanwoo-you <skymill2000@gmail.com>
2020-11-17 11:41:10 +0900

    반복문

---
+++ ex/forLoop.js
@@ -0,0 +1,9 @@
+var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi", "소나타",'페라리'];
+var text = "";
+var i;
+
+for (i = 0; i < cars.length; i++) {	
+	text += cars[i];
+}
+
+console.log(text);
--- ex/objectArray.js
+++ ex/objectArray.js
@@ -25,4 +25,4 @@ var cars = [car1, car2]
 console.log(cars);
 
 //work2 자동차 2번째 요소에 이름을 출력하세요;
-console.log(?);
+console.log(cars[1].name);
