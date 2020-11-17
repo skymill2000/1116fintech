0562d0aaa401624cd11ceb8dfbe72ce128ccf624
gwanwoo-you <skymill2000@gmail.com>
2020-11-17 10:50:04 +0900

    데이터 타입 연습

---
+++ ex/datatype.js
@@ -0,0 +1,21 @@
+
+var length = 16; // Number
+var lastName = "Johnson"; // String
+
+//#in java
+//public int length = 16;
+//public String lastName = "John"
+//public Person person = new Person('관우');
+
+var x = { firstName: "John", lastName: "Doe" }; // Object
+
+var x = 16 + "Volvo";
+console.log(x);
+var y = "16" + "Volvo";
+console.log(y);
+var z = 16 + "Volvo";
+console.log(z);
+var a = "Volvo" + 16;
+console.log(a);
+var b = 16 + 4 + "Volvo";
+console.log(b);
