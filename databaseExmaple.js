0f73c6b6f255d1d5da05e54ebbaab7ca110c2a8a
gwanwoo-you <skymill2000@gmail.com>
2020-11-17 15:40:30 +0900

    데이터베이스 조회 SQL 실습

--- databaseExmaple.js
+++ databaseExmaple.js
@@ -1,16 +1,16 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  host     : 'localhost', //서버의 주소
+  user     : 'root', // 접근 계정 이름
+  password : '1q2w3e4r', // 계정 비밀번호
+  database : 'fintech1116' // 데이터베이스 이름
 });
  
 connection.connect();
  
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+connection.query('SELECT * FROM fintech1116.user', function (error, results, fields) {
   if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+  console.log(results);
 });
  
 connection.end();
