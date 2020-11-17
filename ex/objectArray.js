6a47e8b7d12c0446bfdc9ef15d8444026d764837
gwanwoo-you <skymill2000@gmail.com>
2020-11-17 13:12:32 +0900

    work3 완성

--- ex/objectArray.js
+++ ex/objectArray.js
@@ -34,11 +34,17 @@ var car3 = {
 }
 
 
-var cars = [car1, car2]
+var cars = [car1, car2];
 console.log(cars);
+
 
 //work2 자동차 2번째 요소에 이름을 출력하세요;
 console.log(cars[1].name);
 
 //work3 자동차 이름이 bmw 가 있으면 '!' 출력해주세요; (for / if) 
-
+for(var i = 0; i < cars.length; i++){
+    var car = cars[i];
+    if(car.name == "bmw"){
+        console.log("!");
+    }
+}
