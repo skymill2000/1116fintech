aa97183b983b63efee3afa5abed59eb75cd6c047
gwanwoo-you <skymill2000@gmail.com>
2020-11-18 15:10:51 +0900

    accesstoken 발급 라우터 완성

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
- 
+const request = require('request');
+
 app.set('views', __dirname + '/views');//랜더링할 파일이 있는 디렉토리 
 app.set('view engine', 'ejs'); // 사용하는 뷰 엔진
 
@@ -8,6 +9,9 @@ app.use(express.json()); // JSON 타입의 데이터를 받기위한 설정
 app.use(express.urlencoded({ extended: false })); // urlencoded 타입의 데이터를 받기위한 설정
 
 app.use(express.static(__dirname + '/public')); //디자인 파일이 위치할 정적 요소들을 저장하는 디렉토리
+
+var clientId = "q7kH44ThJwjpvNRg0BbJvE1yxvx5X53DKz1rNgPF" // 클라이언트 아이디 변경
+var clientSecret = "yVT6irMr2h4ZTHzZY7sDpbvhm1nlOzr4nP7DYRVy" // 클라이언트 시크릿 변경
 
 app.get('/test', function(req, res){
   res.render('blank');
@@ -18,6 +22,28 @@ app.get('/signup', function(req, res){
   res.render('signup');
 })
 
+app.get('/authResult', function(req, res){
+  var authCode = req.query.code;
+  console.log("인증코드 : ", authCode)
+  var option = {
+    method : "POST",
+    url : "https://testapi.openbanking.or.kr/oauth/2.0/token",
+    headers : {
+      "Content-Type" : "application/x-www-form-urlencoded; charset=UTF-8"
+    },
+    form : {
+      code : authCode,
+      client_id : clientId,
+      client_secret : clientSecret,
+      redirect_uri : "http://localhost:3000/authResult",
+      grant_type : "authorization_code"
+    }
+  }
+  request(option, function (error, response, body) {
+    console.log(body);
+    res.json(body);
+  });
+})
 
 app.listen(3000, function(){
     console.log('서버가 3000번 포트에서 실행중 입니다.');
