2dd1e11dc3a98dbf2cac3c19d2426e3ac12159ef
gwanwoo-you <skymill2000@gmail.com>
2020-11-18 11:40:11 +0900

    디자인 파일 추가

--- expressServer.js
+++ expressServer.js
@@ -7,6 +7,8 @@ app.set('view engine', 'ejs'); // 사용하는 뷰 엔진
 app.use(express.json()); // JSON 타입의 데이터를 받기위한 설정
 app.use(express.urlencoded({ extended: false })); // urlencoded 타입의 데이터를 받기위한 설정
 
+app.use(express.static(__dirname + '/public')); //디자인 파일이 위치할 정적 요소들을 저장하는 디렉토리
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
@@ -17,6 +19,10 @@ app.get('/test', function (req, res) {
 
 app.get('/ejs', function(req, res){
   res.render('test');
+})
+
+app.get('/designTest', function(req, res){
+  res.render('blank');
 })
 
 app.post('/getTestData', function(req, res){
