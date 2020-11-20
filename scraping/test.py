b78f6ba42b645607ca93e26d0bd3e6201642ddc3
gwanwoo-you <skymill2000@gmail.com>
2020-11-20 15:24:13 +0900

    스크래핑 기본

--- scraping/test.py
+++ scraping/test.py
@@ -2,10 +2,9 @@
 from selenium import webdriver
 driver = webdriver.Chrome('./chromedriver')
 driver.implicitly_wait(3)
-driver.get(
-    'https://newstapa.org/article/ncdGM')
-title = driver.find_element_by_xpath('/html/body/section[1]/div/div[1]/h3')
-body = driver.find_element_by_xpath('//*[@id="journal_article_wrap"]')
+driver.get('http://www.segye.com/newsView/20201120509191')
 
+title = driver.find_element_by_xpath('//*[@id="title_sns"]')
+subject = driver.find_element_by_xpath('//*[@id="article_txt"]/article/p[1]')
 print(title.text)
-print(body.text)
+print(subject.text)
